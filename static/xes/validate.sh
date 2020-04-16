e43bcd5ce868e1e7f31af247fed184281c1d4dae
0b11001111 <19192307+0b11001111@users.noreply.github.com>
2020-04-17 00:12:32 +0200

    clean validation script

--- static/xes/validate.sh
+++ static/xes/validate.sh
@@ -1,10 +1,6 @@
 #!/usr/bin/env bash
 
-BAR=$(xmllint --noout --schema xes-ieee-1849-2016.xsd correct/*.xes)?
-FOO=$?
-
+xmllint --noout --schema xes-ieee-1849-2016.xsd correct/*.xes
 # xmllint --noout --schema xes-ieee-1849-2016.xsd non_parsing/*.xes
 # xmllint --noout --schema xes-ieee-1849-2016.xsd non_parsing/*.xes
 # xmllint --noout --schema xes-ieee-1849-2016.xsd recoverable/*.xes
-
-echo "foo <$FOO> $BAR"

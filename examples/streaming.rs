cf85c75cd9b0b671d942f7bd7519ab0f8e507bdf
0b11001111 <19192307+0b11001111@users.noreply.github.com>
2020-06-04 00:11:02 +0200

    fix imports

--- examples/streaming.rs
+++ examples/streaming.rs
@@ -1,7 +1,6 @@
 use promi::stream::stats::StreamStats;
-use promi::stream::{buffer, stats, xes, Observer, StreamSink};
+use promi::stream::{buffer, stats, xes, Log, Observer, StreamSink};
 use promi::util::expand_static;
-use promi::Log;
 use std::fs;
 use std::io;
 
